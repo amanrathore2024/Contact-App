8847887fad58b9c1c7322e7fdaa3c3a564098ac4
amanrathore2024 <rathoreaman1705@gmail.com>
2025-04-30 20:10:28 +0530

    Initial commit

--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -23,7 +24,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ContactsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   AppNavigation()
+                    Box( modifier = Modifier.padding(innerPadding)) {
+                        AppNavigation()
+                    }
 
                 }
             }
